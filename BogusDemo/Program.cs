2d549676d4415183f38344e757ec074829cef91f
martijn onstwedder <onstwedder@hotmail.com>
2019-03-05 23:03:48 +0100

    initial

---
+++ BogusDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using Bogus;
+using System;
+
+namespace BogusDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var residents = new Faker<Resident>("nl")
+                            .UseSeed(300)
+                            .StrictMode(false)
+                            .Rules((f, o) =>
+                            {
+                                o.Id = f.Random.Replace("###-###-###-###");
+                                o.Name = f.Name.FullName();
+                                o.Street = f.Address.StreetName();
+                                o.Housenumber = f.Address.BuildingNumber();
+                                o.City = f.Address.City();
+                            });
+
+            var residentList = residents.Generate(10);
+
+            foreach (var res in residentList)
+            {
+                res.Print();
+            }
+
+            Console.ReadLine();
+        }
+
+
+    }
+
+}
---
+++ BogusDemo/Resident.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BogusDemo
+{
+    public class Resident
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public string Street { get; set; }
+        public string Housenumber { get; set; }
+
+        public string City { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"{Name}\t{Street}\t{Housenumber}\t{City}\t{Id}");
+        }
+    }
+}
